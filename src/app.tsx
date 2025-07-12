51499da281e93a3fe4725eed07af602c521ed526
Kauan <jailma.kmp@gmail.com>
2025-07-11 22:36:49 -0300

    project setup

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route element={<CreateRoom/>} index/>
+          <Route element={<Room/>} path="/room/:roomId"/>
+          <Route element={<RecordRoomAudio/>} path="/room/:roomId/audio"/> */}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
+
---
+++ src/http/types/get-room-questions-response.ts
@@ -0,0 +1,7 @@
+export type GetRoomsQuestionResponse = Array<{
+  id: string
+  question: string
+  answer: string | null,
+  createdAt: string,
+  isGeneratingAnswer?: boolean,
+}>
